0e3f70fb59eeea96853916c2c429e5c5ab39c249
Sergey Shilin <shilinshtein@gmail.com>
2017-11-27 13:25:47 -0500

    revert model back to VGG16

--- network/model.py
+++ network/model.py
@@ -18,22 +18,22 @@ def get_model_sequential(input_shape=(75, 75, 3)):
     input_angle = Input(shape=[1], name='angle')
     input_angle_norm = BatchNormalization()(input_angle)
 
-    #Conv Layer 1
-    conv1 = Conv2D(32, kernel_size=kernel_size, padding='same')(inputs_bands_norm)
+    # Conv Layer 1
+    conv1 = Conv2D(64, kernel_size=kernel_size, padding='same')(inputs_bands_norm)
     conv1 = BatchNormalization()(conv1)
     conv1 = Activation('relu')(conv1)
-    conv1 = Conv2D(32, kernel_size=kernel_size, padding='same')(conv1)
+    conv1 = Conv2D(64, kernel_size=kernel_size, padding='same')(conv1)
     conv1 = BatchNormalization()(conv1)
     conv1 = Activation('relu')(conv1)
     conv1 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv1)
     conv1 = Dropout(dropout)(conv1)
     # size = 37x37
  
-    #Conv Layer 2
-    conv2 = Conv2D(64, kernel_size=kernel_size, padding='same')(conv1)
+    # Conv Layer 2
+    conv2 = Conv2D(128, kernel_size=kernel_size, padding='same')(conv1)
     conv2 = BatchNormalization()(conv2)
     conv2 = Activation('relu')(conv2)
-    conv2 = Conv2D(64, kernel_size=kernel_size, padding='same')(conv2)
+    conv2 = Conv2D(128, kernel_size=kernel_size, padding='same')(conv2)
     conv2 = BatchNormalization()(conv2)
     conv2 = Activation('relu')(conv2)
     conv2 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv2)
@@ -41,10 +41,13 @@ def get_model_sequential(input_shape=(75, 75, 3)):
     # size = 18x18
 
     # Conv Layer 3
-    conv3 = Conv2D(128, kernel_size=kernel_size, padding='same')(conv2)
+    conv3 = Conv2D(256, kernel_size=kernel_size, padding='same')(conv2)
     conv3 = BatchNormalization()(conv3)
     conv3 = Activation('relu')(conv3)
-    conv3 = Conv2D(128, kernel_size=kernel_size, padding='same')(conv3)
+    conv3 = Conv2D(256, kernel_size=kernel_size, padding='same')(conv3)
+    conv3 = BatchNormalization()(conv3)
+    conv3 = Activation('relu')(conv3)
+    conv3 = Conv2D(256, kernel_size=kernel_size, padding='same')(conv3)
     conv3 = BatchNormalization()(conv3)
     conv3 = Activation('relu')(conv3)
     conv3 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv3)
@@ -52,10 +55,13 @@ def get_model_sequential(input_shape=(75, 75, 3)):
     # size = 9x9
 
     # Conv Layer 4
-    conv4 = Conv2D(256, kernel_size=kernel_size, padding='same')(conv3)
+    conv4 = Conv2D(512, kernel_size=kernel_size, padding='same')(conv3)
     conv4 = BatchNormalization()(conv4)
     conv4 = Activation('relu')(conv4)
-    conv4 = Conv2D(256, kernel_size=kernel_size, padding='same')(conv4)
+    conv4 = Conv2D(512, kernel_size=kernel_size, padding='same')(conv4)
+    conv4 = BatchNormalization()(conv4)
+    conv4 = Activation('relu')(conv4)
+    conv4 = Conv2D(512, kernel_size=kernel_size, padding='same')(conv4)
     conv4 = BatchNormalization()(conv4)
     conv4 = Activation('relu')(conv4)
     conv4 = MaxPooling2D(pool_size=(3, 3), strides=(3, 3))(conv4)
@@ -64,6 +70,9 @@ def get_model_sequential(input_shape=(75, 75, 3)):
 
     # Conv Layer 5
     conv5 = Conv2D(512, kernel_size=kernel_size, padding='same')(conv4)
+    conv5 = BatchNormalization()(conv5)
+    conv5 = Activation('relu')(conv5)
+    conv5 = Conv2D(512, kernel_size=kernel_size, padding='same')(conv5)
     conv5 = BatchNormalization()(conv5)
     conv5 = Activation('relu')(conv5)
     conv5 = Conv2D(512, kernel_size=kernel_size, padding='same')(conv5)
