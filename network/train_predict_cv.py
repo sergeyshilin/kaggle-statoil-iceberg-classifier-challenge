eb4d760b62c2307f3d0ed57c03caa509d952eaae
Sergey Shilin <shilinshtein@gmail.com>
2018-01-02 18:15:12 -0500

    type is fixed

--- network/train_predict_cv.py
+++ network/train_predict_cv.py
@@ -108,7 +108,7 @@ datagen = ImageDataGenerator(
 
 model_info = params.model_factory(input_shape=X_train.shape[1:], inputs_meta=M_train.shape[1])
 model_info.summary()
-model.save_weights(filepath=init_weights)
+model_info.save_weights(filepath=init_weights)
 
 with open(best_model_path, "w") as json_file:
     json_file.write(model_info.to_json())
